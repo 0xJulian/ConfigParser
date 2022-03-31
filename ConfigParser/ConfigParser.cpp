212cef8351081b70dc89f4e67de83d76f948c6c1
qMace <nightbot1144@gmail.com>
2022-03-31 15:40:49 +0200

    refactor

--- ConfigParser/ConfigParser.cpp
+++ ConfigParser/ConfigParser.cpp
@@ -1,5 +1,6 @@
 #include "ConfigWorker.h"
 int main() {
-	ConfigWorker *cfg = new ConfigWorker();
-	cfg->ParseConfig("C:\\Users\\thanner\\source\\repos\\ConfigParser\\x64\\Debug\\Textfile.sus");
+	ConfigWorker* cfg = new ConfigWorker();
+	std::string cfgPath = "C:\\Users\\thanner\\source\\repos\\ConfigParser\\x64\\Debug\\Textfile.sus";
+	cfg->ParseConfig(cfgPath);
 }
