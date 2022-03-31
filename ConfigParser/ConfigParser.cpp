a0492e5d83e93689e3141fb7f990b612ea69e3c2
Julian <79986626+0xJulian@users.noreply.github.com>
2022-03-31 15:47:48 +0200

    Update ConfigParser

--- ConfigParser/ConfigParser.cpp
+++ ConfigParser/ConfigParser.cpp
@@ -1,6 +1,6 @@
 #include "ConfigWorker.h"
 int main() {
 	ConfigWorker* cfg = new ConfigWorker();
-	std::string cfgPath = "C:\\Users\\thanner\\source\\repos\\ConfigParser\\x64\\Debug\\Textfile.sus";
+	std::string cfgPath = "\\Textfile.sus";
 	cfg->ParseConfig(cfgPath);
 }
